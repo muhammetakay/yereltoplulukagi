63d467c52845a9232502311ba781431c815808bf
Muhammet Akay <muhammetakay444@gmail.com>
2023-08-04 15:32:45 +0300

    init

---
+++ database/migrations/2023_07_28_133136_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamp('event_date')->comment('etkinlik tarihi ve saati');
+            $table->string('location')->comment('etkinlik mekanı');
+            $table->string('organizer')->comment('etkinliği düzenleyen kurum veya kişi');
+            $table->longText('description');
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::controller(HomeController::class)->group(function() {
+    Route::get('/', 'index')->name('index');
+    Route::get('contact', 'contact')->name('contact');
+    Route::get('category/{slug?}', 'category')->name('category');
+    Route::get('single/{slug?}', 'single')->name('single');
+});
